d2a9a6deccbcfa01f25827760b391cf5af0281cb
dewansh <dewansh.dt@gmail.com>
2021-05-09 11:08:51 +0530

    touch gestures

--- app.js
+++ app.js
@@ -146,4 +146,62 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            console.log("ArrowLeft");
+            velocity.x = -1;
+            velocity.y = 0;
+        } else {
+            /* right swipe */
+            console.log("ArrowRight");
+            velocity.x = 1;
+            velocity.y = 0;
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+            console.log("ArrowUp");
+            velocity.x = 0;
+            velocity.y = -1;
+        } else { 
+            /* down swipe */
+            console.log("ArrowUp");
+            velocity.x = 0;
+            velocity.y = 1;
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
 main();
