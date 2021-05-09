e3b64bda01a49cd4dfc79972f3211163f63cea9d
dewansh <dewansh.dt@gmail.com>
2021-05-09 11:48:17 +0530

    touch

--- app.js
+++ app.js
@@ -148,71 +148,88 @@ document.addEventListener("keydown", (e) => {
 
 
 // Touch controls
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
+function swipedetect(el, callback){
+  
+  var touchsurface = el,
+  swipedir,
+  startX,
+  startY,
+  distX,
+  distY,
+  threshold = 150, //required min distance traveled to be considered swipe
+  restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+  allowedTime = 300, // maximum time allowed to travel that distance
+  elapsedTime,
+  startTime,
+  handleswipe = callback || function(swipedir){}
 
-var xDown = null;                                                        
-var yDown = null;
+  touchsurface.addEventListener('touchstart', function(e){
+      var touchobj = e.changedTouches[0]
+      swipedir = 'none'
+      dist = 0
+      startX = touchobj.pageX
+      startY = touchobj.pageY
+      startTime = new Date().getTime() // record time when finger first makes contact with surface
+      e.preventDefault()
+  }, false)
 
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
+  touchsurface.addEventListener('touchmove', function(e){
+      e.preventDefault() // prevent scrolling when inside DIV
+  }, false)
 
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
+  touchsurface.addEventListener('touchend', function(e){
+      var touchobj = e.changedTouches[0]
+      distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+      distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+      elapsedTime = new Date().getTime() - startTime // get time elapsed
+      if (elapsedTime <= allowedTime){ // first condition for awipe met
+          if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+              swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+          }
+          else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+              swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+          }
+      }
+      handleswipe(swipedir)
+      e.preventDefault()
+  }, false)
+}
 
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
+//USAGE:
 
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */ 
-            gameOver = false;
-            if (velocity.x != 1) {
-              velocity.x = -1;
-              velocity.y = 0;
-            }
-        } else {
-            /* right swipe */
-            gameOver = false;
-            if (velocity.x != -1) {
-              velocity.x = 1;
-              velocity.y = 0;
-            }
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* up swipe */ 
-            gameOver = false;
-            if (velocity.y != 1) {
-              velocity.x = 0;
-              velocity.y = -1;
-            }
-        } else { 
-            /* down swipe */
-            evt.preventDefault();
-            gameOver = false;
-            if (velocity.y != -1) {
-              velocity.x = 0;
-              velocity.y = 1;
-            }
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+var el = document.getElementById('swipezone');
+swipedetect(el, function(swipedir){
+  // swipedir contains either "none", "left", "right", "top", or "down"
+  switch (swipedir) {
+    case "top":
+      gameOver = false;
+      if (velocity.y != 1) {
+        velocity.x = 0;
+        velocity.y = -1;
+      }
+      break;
+    case "down":
+      gameOver = false;
+      if (velocity.y != -1) {
+        velocity.x = 0;
+        velocity.y = 1;
+      }
+      break;
+    case "left":
+      gameOver = false;
+      if (velocity.x != 1) {
+        velocity.x = -1;
+        velocity.y = 0;
+      }
+      break;
+    case "right":
+      gameOver = false;
+      if (velocity.x != -1) {
+        velocity.x = 1;
+        velocity.y = 0;
+      }
+      break;
+  }
+});
 
 main();
